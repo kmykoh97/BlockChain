9fdf6f29cae274f0831627f4b3b42d34795508da
Koh Meng Yit <kmykoh97@gmail.com>
2018-03-28 09:17:54 -0700

    introducing node.js scripts and blockchain simple codes

---
+++ main.js
@@ -0,0 +1,63 @@
+
+const SHA256 = require('crypto-js/sha256');
+
+class block
+{
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+
+    // to calcuklate the hash property of a block
+    calculateHash() {
+        return SHA256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data)).toString();
+    }
+}
+
+class blockChain
+{
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new block(0, "3/27/2018", "Genesis block", "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash != currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash != previousBlock.hash) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+let testBlock = new blockChain();
+testBlock.addBlock(new block(1, "3/27/2018", { amount : 4 }));
+testBlock.addBlock(new block(2, "3/27/2018", { amount: 10 }));
+
+// console.log('Is BlockChain valid? ' + testBlock.isChainValid());
+// console.log(JSON.stringify(testBlock, null, 4));
